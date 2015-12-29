1a27655fd688bfa2fd0ecc399b5c5f60a0a53bee
Matt Mehlhope <mmehlhope@gmail.com>
2015-12-28 23:31:16 -0800

    Move jquery ui to vendors folder

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -4,9 +4,10 @@
 <meta charset="UTF-8">
 	<title>The Wandering Reader</title>
 	<meta name="description" content="Book Discussion">
-	<link rel="stylesheet" type="text/css" href="/assets/jqueryUI/jquery-ui.css">
-	<link rel="stylesheet" type="text/css" href="/assets/jqueryUI/jquery-ui.css">
-	<link rel="stylesheet" type="text/css" href="/assets/jqueryUI/jquery-ui.structure.css">
-	<script type="text/javascript" src="/assets/jqueryUI/jquery-ui.theme.css"></script>
+	<link rel="stylesheet" type="text/css" href="/assets/vendor/jqueryUI/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="/assets/vendor/jqueryUI/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="/assets/vendor/jqueryUI/jquery-ui.structure.css">
+	<script type="text/javascript" src="/assets/vendor/jqueryUI/jquery-ui.theme.css"></script>
+	<script type="text/javascript" src="/assets/js/bundle.js"></script>
 </head>
 <body>
