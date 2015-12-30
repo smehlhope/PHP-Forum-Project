3dea5ad6892d59bfc01ee4cf1f1f98f351246444
smehlhope <stephanie.mehlhope@gmail.com>
2015-12-29 19:32:02 -0800

    Fixing merge conflict in header.php

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -4,18 +4,11 @@
 <meta charset="UTF-8">
 	<title>The Wandering Reader</title>
 	<meta name="description" content="Book Discussion">
-<<<<<<< HEAD
-	<!-- <link rel="stylesheet" type="text/css" href="/assets/jqueryUI/jquery-ui.css">
-	<link rel="stylesheet" type="text/css" href="/assets/jqueryUI/jquery-ui.css">
-	<link rel="stylesheet" type="text/css" href="/assets/jqueryUI/jquery-ui.structure.css">
-	<script type="text/javascript" src="/assets/jqueryUI/jquery-ui.theme.css"></script> -->
-=======
 	<link rel="stylesheet" type="text/css" href="/assets/vendor/jqueryUI/jquery-ui.css">
 	<link rel="stylesheet" type="text/css" href="/assets/vendor/jqueryUI/jquery-ui.css">
 	<link rel="stylesheet" type="text/css" href="/assets/vendor/jqueryUI/jquery-ui.structure.css">
 	<link rel="stylesheet" type="text/css" href="/assets/vendor/jqueryUI/jquery-ui.theme.css"></script>
 	<script type="text/javascript" src="/assets/js/application.js"></script>
 	<?php include($_SERVER['DOCUMENT_ROOT'] . '/application/helpers/include_page_script.php') ?>
->>>>>>> origin/master
 </head>
 <body>
